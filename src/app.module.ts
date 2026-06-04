276ffd7172975f4177147de006a994713180a6d9
Anthony <anthonyjosue0016@gmail.com>
2026-06-04 02:52:13 -0500

    proyecto creado starscreen

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { CategoriesModule } from './categories/categories.module';
+import { PeliculasModule } from './peliculas/peliculas.module';
+import { EntradasModule } from './entradas/entradas.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    CategoriesModule,
+    PeliculasModule,
+    EntradasModule,
+  ],
+})
+export class AppModule {}
